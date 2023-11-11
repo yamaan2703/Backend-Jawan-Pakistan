ea171f2de2ba96390df04f820e3bcd3920dc8808
yamaan2703 <115937602+yamaan2703@users.noreply.github.com>
2023-11-11 03:37:46 -0800

    main

--- Backend/controllers/authcontroller.js
+++ Backend/controllers/authcontroller.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken")
 const { SendResponse } = require("../helpers/helper");
 
 const AuthController = {
-    signup: async (req, res) => {
+        signup: async (req, res) => {
         console.log(req.body)
         try {
             let {userName, email, password } = req.body
--- Frontend/my-app/src/pages/sigUp.js
+++ Frontend/my-app/src/pages/sigUp.js
@@ -1,58 +1,183 @@
+// import { useNavigate } from "react-router-dom";
+// import BackBtn from "../components/backBtn";
+// import BackInp from "../components/backInp";
+// import axios from "axios";
+// import { useState } from "react";
+
+// export default function SignUp() {
+//   const navigate = useNavigate();
+//   const [sendSignUp, setSendSignUp] = useState({
+//     name: "",
+//     email: "",
+//     password: "",
+//   });
+
+//   const GoToLogin = () => {
+//     navigate("/login");
+//   };
+
+//   const SignUp = () => {
+//     console.log(sendSignUp);
+//     axios
+//       .post("http://localhost:5000/auth/signup", sendSignUp)
+//       .then((res) => {
+//         console.log(res.data);
+//         setSendSignUp({
+//           name: "",
+//           email: "",
+//           password: "",
+//         });
+//         console.log(setSendSignUp);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   };
+
+//   const handleInputChange = (key, val) => {
+//     setSendSignUp({ ...sendSignUp, [key]: val });
+//   };
+//   return (
+//     <>
+//       <div className="h-screen bg-sky-500 flex justify-center items-center">
+//         <div className="w-[400px] bg-[rgba(255,255,255,0.19)] p-10 rounded-lg text-center">
+//           <div className="text-5xl text-white font-bold">
+//             <h1>SignUp</h1>
+//           </div>
+
+//           <div className="my-2">
+//             <div className="my-1">
+//               <BackInp
+//                 // name="name"
+//                 // onChange={(e) => handleInputChange("name", e.target.value)}
+//                 value={sendSignUp.name}
+//                 onChange={(e) =>
+//                   setSendSignUp({ ...sendSignUp, name: e.target.value })
+//                 }
+//                 label="Name"
+//               />
+//             </div>
+
+//             <div className="my-1">
+//               <BackInp
+//                 // name="email"
+//                 // onChange={(e) => handleInputChange("email", e.target.value)}
+//                 value={sendSignUp.email}
+//                 onChange={(e) =>
+//                   setSendSignUp({ ...sendSignUp, email: e.target.value })
+//                 }
+//                 label="Email"
+//               />
+//             </div>
+
+//             <div className="my-1">
+//               <BackInp
+//                 // name="password"
+//                 // onChange={(e) => handleInputChange("password", e.target.value)}
+//                 value={sendSignUp.password}
+//                 onChange={(e) =>
+//                   setSendSignUp({ ...sendSignUp, password: e.target.value })
+//                 }
+//                 label="Password"
+//               />
+//             </div>
+
+//             <div className="my-1">
+//               <BackBtn onClick={SignUp} label="SignUp" />
+//             </div>
+
+//             <div className="my-1">
+//               <BackBtn onClick={GoToLogin} label="Go To Login" />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+
 import { useNavigate } from "react-router-dom";
 import BackBtn from "../components/backBtn";
 import BackInp from "../components/backInp";
+import axios from "axios";
+import { useState } from "react";
 
-export default function SignUp () {
-  const navigate = useNavigate()
-  const [sendSignUp, setSendSignUp] = ({
-
-  })
+export default function SignUp() {
+  const navigate = useNavigate();
+  const [sendSignUp, setSendSignUp] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
   const GoToLogin = () => {
-    navigate("/login")
-  }
+    navigate("/login");
+  };
 
-  // const SignUp = () => {
-  //   axios.
-  // }
-    return (
-        <>
-        <div className="h-screen bg-sky-500 flex justify-center items-center">
-            <div className="w-[400px] bg-[rgba(255,255,255,0.19)] p-10 rounded-lg text-center">
+  const handleInputChange = (key, val) => {
+    setSendSignUp({ ...sendSignUp, [key]: val });
+  };
 
-            <div className="text-5xl text-white font-bold">
-            <h1>SignUp</h1>
-          </div>
- 
-           <div className="my-2">
-            <div className="my-1">
-            <BackInp 
-            label="Name"/>
-            </div>
+  const SignUp = () => {
+    console.log(sendSignUp);
+    axios
+      .post("http://localhost:5000/auth/signup", sendSignUp)
+      .then((res) => {
+        console.log(res.data);
+        setSendSignUp({
+          name: "",
+          email: "",
+          password: "",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-            <div className="my-1">
-            <BackInp 
-            label="Email"/>
-            </div>
-
-            <div className="my-1">
-            <BackInp 
-            label="Password"/>
-            </div>
-
-            <div className="my-1">
-              <BackBtn 
-              label="SignUp"/>
-            </div>
-
-            <div className="my-1">
-              <BackBtn 
-              onClick={GoToLogin}
-              label="Go To Login"/>
-            </div>
-           </div>
-            </div>
+  return (
+    <div className="h-screen bg-sky-500 flex justify-center items-center">
+      <div className="w-[400px] bg-[rgba(255,255,255,0.19)] p-10 rounded-lg text-center">
+        <div className="text-5xl text-white font-bold">
+          <h1>SignUp</h1>
         </div>
-        </>
-    )
+
+        <div className="my-2">
+          <div className="my-1">
+            <BackInp
+              value={sendSignUp.name}
+              onChange={(e) => handleInputChange("name", e.target.value)}
+              label="Name"
+            />
+          </div>
+
+          <div className="my-1">
+            <BackInp
+              value={sendSignUp.email}
+              onChange={(e) => handleInputChange("email", e.target.value)}
+              label="Email"
+            />
+          </div>
+
+          <div className="my-1">
+            <BackInp
+              value={sendSignUp.password}
+              onChange={(e) => handleInputChange("password", e.target.value)}
+              label="Password"
+            />
+          </div>
+
+          <div className="my-1">
+            <BackBtn onClick={SignUp} label="SignUp" />
+          </div>
+
+          <div className="my-1">
+            <BackBtn onClick={GoToLogin} label="Go To Login" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
